da8290fd6eef93ce6dfaaf78294a6f56d475a081
David Lindgren Kamali <david.lindgren.kamali@gmail.com>
2022-05-23 23:21:29 +0200

    Moved code from main to ObjectDetection.py, converts screenshot to cv format and added support for PyQt5

---
+++ ObjectDetection.py
@@ -0,0 +1,51 @@
+import cv2 as cv
+import numpy as np
+import pyautogui
+from win32api import GetSystemMetrics
+from Screenshot import Screenshot as ss
+import win32api, win32con
+
+class ObjectDetection:
+    @staticmethod
+    def find_object():
+        screenshot = ss.capture_screen()
+
+        photo_img = cv.imread('Objects/CSGO/csgo.jpg', cv.IMREAD_UNCHANGED)
+        object_img = cv.imread('Objects/CSGO/terrorist.jpg', cv.IMREAD_UNCHANGED)
+
+        result = cv.matchTemplate(screenshot, object_img, cv.TM_CCOEFF_NORMED)
+
+        # get best match pos
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+
+        ward_w = object_img.shape[1]
+        ward_h = object_img.shape[0]
+
+        threshold = 0.5
+        loc = np.where(result >= threshold)
+
+        loc = list(zip(*loc[::-1]))
+
+        rectangles = []
+        for l in loc:
+            rect = [int(l[0]), int(l[1]), ward_w, ward_h]
+            rectangles.append(rect)
+
+        rectangles, weights = cv.groupRectangles(rectangles, 1, 0.5)
+
+        if len(rectangles):
+            print("Found images")
+            line_color = (255, 0, 0)
+            line_type = cv.LINE_4
+            marker_color = (0, 0, 255)
+            marker_type = cv.MARKER_SQUARE
+
+            for (x, y, w, h) in rectangles:
+                top_left = (x, y)
+                bottom_right = (x + w, y + h)
+                cv.rectangle(photo_img, top_left, bottom_right, marker_color, marker_type)
+                win32api.SetCursorPos((x + int(w / 2), y + int(h / 2)))
+
+            cv.imshow('Matches', screenshot)
+        else:
+            print("No image found")
--- Screenshot.py
+++ Screenshot.py
@@ -1,5 +1,6 @@
 import mss
 import numpy as np
+import cv2 as cv
 
 
 class Screenshot:
@@ -8,4 +9,5 @@ class Screenshot:
         with mss.mss() as mss_instance:
             monitor_1 = mss_instance.monitors[1]
             screenshot = np.array(mss_instance.grab(monitor_1))
+            screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
             return screenshot
