67d5716e6216b445681f2fb7eae494b2e20fe921
David Lindgren Kamali <david.lindgren.kamali@gmail.com>
2022-06-01 09:01:19 +0200

    ObjectDetectionInterface.py: added qcombobox and radiobuttons. Also split up the code in functions

--- ObjectDetectionInterface.py
+++ ObjectDetectionInterface.py
@@ -28,38 +28,35 @@ class Ui_MainWindow(object):
         MainWindow.resize(1200, 900)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.screen = QtWidgets.QLabel(self.centralwidget)
-        self.screen.setGeometry(QtCore.QRect(4, -1, 720, 405))
-        self.screen.setText("")
-        self.screen.setScaledContents(True)
-        self.screen.setObjectName("screen")
-        black = QPixmap(16,16)
-        black.fill(Qt.black)
 
-        self.black = black
+        self.setScreen()
+        self.setThreshold()
+        self.setRadioButtons()
 
-        self.screen.setPixmap(self.black)
-        self.Threshold = QtWidgets.QSlider(self.centralwidget)
-        self.Threshold.setGeometry(QtCore.QRect(10, 450, 160, 16))
-        self.Threshold.setMaximum(100)
-        self.Threshold.setOrientation(QtCore.Qt.Horizontal)
-        self.Threshold.setObjectName("Threshold")
-        self.Threshold.valueChanged.connect(self.update_threshold)
-        self.thresholdLabel = QtWidgets.QLabel(self.centralwidget)
-        self.thresholdLabel.setGeometry(QtCore.QRect(200, 450, 160, 16))
-        self.thresholdLabel.setObjectName("thresholdLabel")
-        self.StartStop_ObjectDetection = QtWidgets.QPushButton(self.centralwidget)
-        self.StartStop_ObjectDetection.setGeometry(QtCore.QRect(170, 500, 131, 31))
-        self.StartStop_ObjectDetection.setObjectName("StartStop_ObjectDetection")
-        self.StartStop_ObjectDetection.clicked.connect(self.startStopButton)
-        self.StartStop_ObjectDetection.setEnabled(False)
+        self.positionLabel = QtWidgets.QLabel(self.centralwidget)
+        self.positionLabel.setGeometry(QtCore.QRect(10, 550, 160, 16))
+        self.positionLabel.setObjectName("positionLabel")
+
+        self.positionBox = QtWidgets.QComboBox(self.centralwidget)
+        self.positionBox.addItem("UpperLeft")
+        self.positionBox.addItem("UpperRight")
+        self.positionBox.addItem("LowerLeft")
+        self.positionBox.addItem("LowerRight")
+        self.positionBox.setGeometry(80, 545, 160, 32)
+
+
+        self.startStop_ObjectDetection = QtWidgets.QPushButton(self.centralwidget)
+        self.startStop_ObjectDetection.setGeometry(QtCore.QRect(170, 600, 131, 31))
+        self.startStop_ObjectDetection.setObjectName("StartStop_ObjectDetection")
+        self.startStop_ObjectDetection.clicked.connect(self.startStopButton)
+        self.startStop_ObjectDetection.setEnabled(False)
         self.toolButton = QtWidgets.QToolButton(self.centralwidget)
-        self.toolButton.setGeometry(QtCore.QRect(10, 500, 121, 31))
+        self.toolButton.setGeometry(QtCore.QRect(10, 600, 121, 31))
         self.toolButton.setObjectName("toolButton")
         self.toolButton.clicked.connect(self.get_object)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 600, 18))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 700, 18))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -71,47 +68,91 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.StartStop_ObjectDetection.setText(_translate("MainWindow", "Start"))
+        self.startStop_ObjectDetection.setText(_translate("MainWindow", "Start"))
         self.toolButton.setText(_translate("MainWindow", "Choose file"))
-        self.thresholdLabel.setText(_translate("MainWindow", str(self.Threshold.value()) / 100))
+        self.thresholdLabel.setText(_translate("MainWindow", str(self.threshold.value() / 100)))
+        self.modeLabel.setText(_translate("MainWindow", "Mode:"))
+        self.defaultLabel.setText(_translate("Main", "Default"))
+        self.fpsLabel.setText(_translate("Main", "FPS"))
+        self.positionLabel.setText(_translate("Main", "Position:"))
 
+    def setScreen(self):
+        self.screen = QtWidgets.QLabel(self.centralwidget)
+        self.screen.setGeometry(QtCore.QRect(4, -1, 720, 405))
+        self.screen.setText("")
+        self.screen.setScaledContents(True)
+        self.screen.setObjectName("screen")
+        black = QPixmap(16, 16)
+        black.fill(Qt.black)
+        self.black = black
+        self.screen.setPixmap(self.black)
+
+    def setThreshold(self):
+        self.threshold = QtWidgets.QSlider(self.centralwidget)
+        self.threshold.setGeometry(QtCore.QRect(10, 450, 160, 16))
+        self.threshold.setMaximum(100)
+        self.threshold.setOrientation(QtCore.Qt.Horizontal)
+        self.threshold.setObjectName("Threshold")
+        self.threshold.valueChanged.connect(self.update_threshold)
+        self.thresholdLabel = QtWidgets.QLabel(self.centralwidget)
+        self.thresholdLabel.setGeometry(QtCore.QRect(200, 450, 160, 16))
+        self.thresholdLabel.setObjectName("thresholdLabel")
+
+    def setRadioButtons(self):
+        self.modeLabel = QtWidgets.QLabel(self.centralwidget)
+        self.modeLabel.setGeometry(10, 500, 160, 16)
+        self.modeLabel.setObjectName("modeLabel")
+
+        self.radioButton_mode_default = QtWidgets.QRadioButton(self.centralwidget)
+        self.radioButton_mode_default.setGeometry(QtCore.QRect(70, 500, 95, 20))
+        self.defaultLabel = QtWidgets.QLabel(self.centralwidget)
+        self.defaultLabel.setGeometry(QtCore.QRect(100, 500, 160, 16))
+        self.defaultLabel.setObjectName("defaultLabel")
+
+        self.radioButton_mode_FPS = QtWidgets.QRadioButton(self.centralwidget)
+        self.radioButton_mode_FPS.setGeometry(QtCore.QRect(170, 500, 95, 20))
+        self.fpsLabel = QtWidgets.QLabel(self.centralwidget)
+        self.fpsLabel.setGeometry(QtCore.QRect(200, 500, 160, 16))
+        self.fpsLabel.setObjectName("fpsLabel")
 
     def get_object(self):
         Tk().withdraw()
         self.objectpath = askopenfilename()
-        self.StartStop_ObjectDetection.setEnabled(True)
+        self.startStop_ObjectDetection.setEnabled(True)
 
 
     def update_image_slot(self, nparray):
 
-        PILimage = Image.fromarray(nparray)
+        pilImage = Image.fromarray(nparray)
         cvImage = cv2.cvtColor(nparray, cv2.COLOR_RGB2BGR)
 
         ConvertToQtFormat = QImage(cvImage.data, cvImage.shape[1], cvImage.shape[0], QImage.Format_RGB888)
         Pic = ConvertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
         self.screen.setPixmap(QPixmap.fromImage(Pic))
 
+
     def black_screen(self):
         self.screen.setPixmap(self.black)
 
+
     def startStopButton(self):
-        if self.StartStop_ObjectDetection.text() == "Start":
-            self.objectdetetction = ObjectDetection(self.Threshold.value() / 100, self.objectpath)
+        if self.startStop_ObjectDetection.text() == "Start":
+            self.objectdetetction = ObjectDetection(self.threshold.value() / 100, self.objectpath)
             self.Worker = Worker()
             self.Worker.start()
             self.Worker.ImageUpdate.connect(self.update_image_slot)
             self.Worker.BlackScreen.connect(self.black_screen)
-            self.StartStop_ObjectDetection.setText("Stop")
+            self.startStop_ObjectDetection.setText("Stop")
 
-        elif self.StartStop_ObjectDetection.text() == "Stop":
+        elif self.startStop_ObjectDetection.text() == "Stop":
             self.Worker.stop()
-            self.StartStop_ObjectDetection.setText("Start")
+            self.startStop_ObjectDetection.setText("Start")
 
     def update_threshold(self):
-        self.thresholdLabel.setText(str(self.Threshold.value() / 100))
+        self.thresholdLabel.setText(str(self.threshold.value() / 100))
 
         if hasattr(self, 'objectdetetction'):
-            self.objectdetetction.threshold = self.Threshold.value() / 100
+            self.objectdetetction.threshold = self.threshold.value() / 100
 
 
 
