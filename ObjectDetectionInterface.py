93ce48ccf5c12d085ff3ebbe2c38c235d81d309a
David Lindgren Kamali <david.lindgren.kamali@gmail.com>
2022-06-02 09:37:35 +0200

    ObjectDetectionInterface.py: Continued dividing code into functions. Added support for a corner window that shows the screen in a corner of users choice

--- ObjectDetectionInterface.py
+++ ObjectDetectionInterface.py
@@ -21,45 +21,79 @@ import numpy as np
 from ObjectDetection import ObjectDetection
 
 
+class CornerWindow(QWidget):
+    def __init__(self, x, y):
+        super().__init__()
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.run_objectdetection = True
+        self.x = 0
+        self.y = 0
+
+        self.setLayout(layout)
+        self.setFixedSize(320, 180)
+        self.move(x, y)
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
+
+        self.screen = QLabel()
+        self.Worker = Worker()
+        self.Worker.ImageUpdate.connect(self.update_image_slot)
+        black = QPixmap(360, 1440)
+        black.fill(Qt.black)
+        self.screen.setPixmap(black)
+        layout.addWidget(self.screen)
+
+    def update_image_slot(self, nparray):
+        cvImage = cv2.cvtColor(nparray, cv2.COLOR_RGB2BGR)
+
+        ConvertToQtFormat = QImage(cvImage.data, cvImage.shape[1], cvImage.shape[0], QImage.Format_RGB888)
+        Pic = ConvertToQtFormat.scaled(320, 180, Qt.KeepAspectRatio)
+        self.screen.setPixmap(QPixmap.fromImage(Pic))
+
+    def move_window(self):
+        self.move(self.x, self.y)
+
+
+
 class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        super(Ui_MainWindow, self).__init__()
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1200, 900)
+
+    def __init__(self):
+        super().__init__()
+        self.corner_x = 0
+        self.corner_y = 0
         self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.startStop_ObjectDetection = QtWidgets.QPushButton(self.centralwidget)
+        self.toolButton = QtWidgets.QToolButton(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.corner = CornerWindow(self.corner_x, self.corner_y)
+
+    def setupUi(self, MainWindow):
+
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(700, 700)
+
         self.centralwidget.setObjectName("centralwidget")
 
         self.setScreen()
         self.setThreshold()
         self.setRadioButtons()
+        self.setPositionBox()
 
-        self.positionLabel = QtWidgets.QLabel(self.centralwidget)
-        self.positionLabel.setGeometry(QtCore.QRect(10, 550, 160, 16))
-        self.positionLabel.setObjectName("positionLabel")
-
-        self.positionBox = QtWidgets.QComboBox(self.centralwidget)
-        self.positionBox.addItem("UpperLeft")
-        self.positionBox.addItem("UpperRight")
-        self.positionBox.addItem("LowerLeft")
-        self.positionBox.addItem("LowerRight")
-        self.positionBox.setGeometry(80, 545, 160, 32)
-
-
-        self.startStop_ObjectDetection = QtWidgets.QPushButton(self.centralwidget)
         self.startStop_ObjectDetection.setGeometry(QtCore.QRect(170, 600, 131, 31))
         self.startStop_ObjectDetection.setObjectName("StartStop_ObjectDetection")
         self.startStop_ObjectDetection.clicked.connect(self.startStopButton)
         self.startStop_ObjectDetection.setEnabled(False)
-        self.toolButton = QtWidgets.QToolButton(self.centralwidget)
+
         self.toolButton.setGeometry(QtCore.QRect(10, 600, 121, 31))
         self.toolButton.setObjectName("toolButton")
         self.toolButton.clicked.connect(self.get_object)
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
+
         self.menubar.setGeometry(QtCore.QRect(0, 0, 700, 18))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
         self.retranslateUi(MainWindow)
@@ -105,25 +139,75 @@ class Ui_MainWindow(object):
 
         self.radioButton_mode_default = QtWidgets.QRadioButton(self.centralwidget)
         self.radioButton_mode_default.setGeometry(QtCore.QRect(70, 500, 95, 20))
+        self.radioButton_mode_default.setChecked(True)
+        self.radioButton_mode_default.setObjectName("Default")
+        self.radioButton_mode_default.toggled.connect(self.set_mode)
+        self.mode = self.radioButton_mode_default.objectName()
+
         self.defaultLabel = QtWidgets.QLabel(self.centralwidget)
         self.defaultLabel.setGeometry(QtCore.QRect(100, 500, 160, 16))
         self.defaultLabel.setObjectName("defaultLabel")
 
+
         self.radioButton_mode_FPS = QtWidgets.QRadioButton(self.centralwidget)
         self.radioButton_mode_FPS.setGeometry(QtCore.QRect(170, 500, 95, 20))
+        self.radioButton_mode_default.setObjectName("FPS")
         self.fpsLabel = QtWidgets.QLabel(self.centralwidget)
         self.fpsLabel.setGeometry(QtCore.QRect(200, 500, 160, 16))
         self.fpsLabel.setObjectName("fpsLabel")
+
+    def set_mode(self):
+
+        if self.radioButton_mode_default.isChecked():
+            self.mode = self.radioButton_mode_default.objectName()
+            print("0")
+
+        elif self.radioButton_mode_FPS.isChecked():
+            self.mode = self.radioButton_mode_FPS.objectName()
+            print("1")
+
 
     def get_object(self):
         Tk().withdraw()
         self.objectpath = askopenfilename()
         self.startStop_ObjectDetection.setEnabled(True)
 
+    def setPositionBox(self):
+
+        self.positionLabel = QtWidgets.QLabel(self.centralwidget)
+        self.positionLabel.setGeometry(QtCore.QRect(10, 550, 160, 16))
+        self.positionLabel.setObjectName("positionLabel")
+
+        self.positionBox = QtWidgets.QComboBox(self.centralwidget)
+        self.positionBox.addItem("UpperLeft")
+        self.positionBox.addItem("UpperRight")
+        self.positionBox.addItem("LowerLeft")
+        self.positionBox.addItem("LowerRight")
+        self.positionBox.setGeometry(80, 545, 160, 32)
+        self.positionBox.activated.connect(self.set_position)
+
+    def set_position(self, index):
+        if index == 0:
+            self.corner.x = 0
+            self.corner.y = 0
+
+        elif index == 1:
+            self.corner.x = 1920 - 320
+            self.corner.y = 0
+
+        elif index == 2:
+            self.corner.x = 0
+            self.corner.y = 1080 - 180
+
+        elif index == 3:
+            self.corner.x = 1920 - 320
+            self.corner.y = 1080 - 180
+
+        self.corner.move_window()
+
 
     def update_image_slot(self, nparray):
 
-        pilImage = Image.fromarray(nparray)
         cvImage = cv2.cvtColor(nparray, cv2.COLOR_RGB2BGR)
 
         ConvertToQtFormat = QImage(cvImage.data, cvImage.shape[1], cvImage.shape[0], QImage.Format_RGB888)
@@ -134,9 +218,10 @@ class Ui_MainWindow(object):
     def black_screen(self):
         self.screen.setPixmap(self.black)
 
-
     def startStopButton(self):
         if self.startStop_ObjectDetection.text() == "Start":
+            self.corner.show()
+            self.corner.Worker.start()
             self.objectdetetction = ObjectDetection(self.threshold.value() / 100, self.objectpath)
             self.Worker = Worker()
             self.Worker.start()
@@ -146,14 +231,16 @@ class Ui_MainWindow(object):
 
         elif self.startStop_ObjectDetection.text() == "Stop":
             self.Worker.stop()
+            self.corner.Worker.stop()
             self.startStop_ObjectDetection.setText("Start")
+            self.corner.run_objectdetection = False
+            self.corner.close()
 
     def update_threshold(self):
         self.thresholdLabel.setText(str(self.threshold.value() / 100))
 
         if hasattr(self, 'objectdetetction'):
             self.objectdetetction.threshold = self.threshold.value() / 100
-
 
 
 class Worker(QThread):
